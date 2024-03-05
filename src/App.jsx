dcdf168e256e3ff354392389552ab7dcdbc73e30
Kaushik Saha <116095138+Kaushik1766@users.noreply.github.com>
2024-03-05 18:44:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css";
+import Navigation from "./Components/Nav";
+import Herosection from './Components/Hero';
+
+const App = () => {
+  return(
+   <div>
+         <Navigation />
+         <Herosection />
+         
+   </div>
+  );
+};
+export default App ;
